b8118b0be6364a908f77132d41fd356c04364fa1
ks676 <70753150+ks676@users.noreply.github.com>
2022-02-04 15:32:36 -0800

    renaming test_generator_master dataset + fixing rounding bugs that were leading to dropped rows

--- test_generator_concentrations_and_rr.py
+++ test_generator_concentrations_and_rr.py
@@ -17,24 +17,37 @@ def main():
     conc = pd.read_csv('/Users/kiratsingh/Desktop/research/india_coal/health/output/mundra_conc.csv',
                        index_col=False)
 
-    print(len(conc['C_star_i']))
+    # filter out empty rows
+    conc = conc[conc['C_i'].notnull()]
 
     # round C_i concentrations to the same levels as in the rr curves:
     # 1. if <=1, two decimal places
     # 2. if <=10, 1 decimal place
     # 3. if >10, 0 decimal places
+    # 4. if > 100, nearest tens
+    # 5. if > 1000, nearest hundreds
 
     round_2 = conc[conc['C_i'] < 1.001]
     round_2 = round_2.round({'C_i': 2})
 
-    round_1 = conc[(conc['C_i'] < 10) & (conc['delta_C_i'] >= 1.001)]
+    round_1 = conc[(conc['C_i'] < 10) & (conc['C_i'] >= 1.001)]
     round_1 = round_1.round({'C_i': 1})
 
-    round_0 = conc[conc['C_i'] >= 10]
+    round_0 = conc[(conc['C_i'] < 100) & (conc['C_i'] >= 10)]
     round_0 = round_0.round({'C_i': 0})
 
+    round_tens = conc[(conc['C_i'] < 1000) & (conc['C_i'] >= 100)]
+    round_tens['C_i'] = round_tens['C_i'] / 10
+    round_tens = round_tens.round({'C_i': 0})
+    round_tens['C_i'] = round_tens['C_i'] * 10
+
+    round_hundreds = conc[conc['C_i'] >= 1000]
+    round_hundreds['C_i'] = round_hundreds['C_i']/100
+    round_hundreds = round_hundreds.round({'C_i': 0})
+    round_hundreds['C_i'] = round_hundreds['C_i'] * 100
+
     # recombine
-    conc = round_0.append(round_1).append(round_2)
+    conc = pd.concat([round_0, round_1, round_2, round_tens, round_hundreds], ignore_index=True)
 
     # similarly round the C_star_i values (both will be mapped to rr data)
 
@@ -44,37 +57,41 @@ def main():
     round_1 = conc[(conc['C_star_i'] < 10) & (conc['C_star_i'] >= 1.001)]
     round_1 = round_1.round({'C_star_i': 1})
 
-    round_0 = conc[conc['C_star_i'] >= 10]
+    round_0 = conc[(conc['C_star_i'] < 100) & (conc['C_star_i'] >= 10)]
     round_0 = round_0.round({'C_star_i': 0})
 
+    round_tens = conc[(conc['C_star_i'] < 1000) & (conc['C_star_i'] >= 100)]
+    round_tens['C_star_i'] = round_tens['C_star_i']/10
+    round_tens = round_tens.round({'C_star_i': 0})
+    round_tens['C_star_i'] = round_tens['C_star_i']*10
+
+    round_hundreds = conc[conc['C_star_i'] >= 1000]
+    round_hundreds['C_star_i'] = round_hundreds['C_star_i']/100
+    round_hundreds = round_hundreds.round({'C_star_i': 0})
+    round_hundreds['C_star_i'] = round_hundreds['C_star_i'] * 100
+
     # recombine
-    conc = round_0.append(round_1).append(round_2)
+    conc = pd.concat([round_0, round_1, round_2, round_tens, round_hundreds], ignore_index=True)
 
     # convert conc columns to string
     conc['C_star_i'] = conc['C_star_i'].astype(str)
     conc['C_i'] = conc['C_i'].astype(str)
 
-    print(len(conc['C_star_i']))
-
     # open rr_by_state_and_endpoint dataset
     rr = pd.read_csv('/Users/kiratsingh/Desktop/research/india_coal/health/output/rr_by_state_and_endpoint.csv',
                        index_col=False)
 
-    round_2 = rr[rr['conc'] < 1.001]
-    round_2 = round_2.round({'conc': 2})
-
-    round_1 = rr[(rr['conc'] < 10) & (rr['conc'] >= 1.001)]
-    round_1 = round_1.round({'conc': 1})
+    # here the only rounding we need to do is to int for values >= 10
+    round_none = rr[rr['conc'] < 10]
 
     round_0 = rr[rr['conc'] >= 10]
     round_0 = round_0.round({'conc': 0})
 
     # recombine
-    rr = round_0.append(round_1).append(round_2)
+    rr = pd.concat([round_0, round_none], ignore_index=True)
 
     # convert conc to string
     rr['conc'] = rr['conc'].astype(str)
-
 
     # join on state id and C_i
     conc = pd.merge(conc, rr[['endpoint',
@@ -88,9 +105,6 @@ def main():
     conc = conc.rename(columns={"mean_rr": "mean_rr_C_i",
                                 "endpoint": "endpoint_left"})
 
-    #conc = conc[conc.conc.isnull()]
-
-    #print(len(conc['C_star_i']))
 
     # join on state id and C_i
     conc = pd.merge(conc, rr[['endpoint',
@@ -104,11 +118,13 @@ def main():
     # rename mean_rr
     conc = conc.rename(columns={"mean_rr": "mean_rr_C_star_i"})
 
-    conc = conc[conc['endpoint'] == 'cvd_ihd']
+    # drop extra columns
+    conc = conc.drop(columns=['endpoint_left',
+                              'conc_x',
+                              'conc_y'])
 
+    # export as csv for use in later scripts
     conc.to_csv('/Users/kiratsingh/Desktop/research/india_coal/health/output/test_join.csv',
-                index=False)
-
-
+                 index=False)
 
 main()
