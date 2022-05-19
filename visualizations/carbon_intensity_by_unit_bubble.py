e9a7db2be3d4f1dfb105170134a705aa2a7ce967
ks676 <70753150+ks676@users.noreply.github.com>
2022-05-19 13:48:04 -0700

    adding script to visualize top mortality intensity units on map

--- visualizations/carbon_intensity_by_unit_bubble.py
+++ visualizations/carbon_intensity_by_unit_bubble.py
@@ -9,30 +9,32 @@ import numpy as np
 import plotly.graph_objects as go
 import matplotlib
 
+KG_TONNES = 0.001
+
 def main():
 
     # import dataset containing mortality by unit
     unit_mort = gdf.read_file("/Users/kiratsingh/Desktop/research/india_coal/health/output/visualization_tables/unit_mortality_and_ef.csv")
     unit_mort.crs = 'epsg:4326'
     unit_mort.geometry = gdf.points_from_xy(unit_mort.longitude, unit_mort.latitude)
-    unit_mort["sum_delta_M_ij"] = unit_mort["sum_delta_M_ij"].astype(float)
+    unit_mort["annual_plant_CO2_kg"] = unit_mort["annual_plant_CO2_kg"].astype(float)
     unit_mort["annual_unit_gen_GWh"] = unit_mort["annual_unit_gen_GWh"].astype(float)
-    unit_mort["mort_intensity"] = unit_mort["sum_delta_M_ij"]/unit_mort["annual_unit_gen_GWh"]
+    unit_mort["carbon_intensity"] = (unit_mort["annual_plant_CO2_kg"]/unit_mort["annual_unit_gen_GWh"])* KG_TONNES
 
     # import states shapefile
     states = gdf.read_file("/Users/kiratsingh/Desktop/research/india_coal/health/input/maps/2011_states.shp")
 
     india_map = states.plot()
     unit_mort.plot(ax=india_map,
-                   column=unit_mort["mort_intensity"],
-                   markersize= unit_mort["mort_intensity"]*30,
+                   column=unit_mort["carbon_intensity"],
+                   markersize= unit_mort["carbon_intensity"]*0.05,
                    cmap="inferno_r",
                    legend=True)
 
-    plt.title('Premature Mortality Intensity per Unit (deaths/GWh)', fontsize=10)
+    #plt.title('Carbon Dioxide Emissions Intensity per Unit (tonnesCO2/GWh)', fontsize=10)
     plt.xlabel("Longitude (°E)")
     plt.ylabel("Latitude (°N)")
-    plt.savefig("/Users/kiratsingh/Documents/coal_health_effects/visualizations/plots/unit_bubble_mort_intensity.png",
+    plt.savefig("/Users/kiratsingh/Documents/coal_health_effects/visualizations/plots/unit_bubble_carbon_intensity.png",
                 dpi=2400)
 
     plt.show()
---
+++ visualizations/top_25_mort_by_gen.py
@@ -0,0 +1,39 @@
+# Plotting 64 units linked to 25% of cumulative mortality
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gdf
+import plotly.express as px
+import numpy as np
+import plotly.graph_objects as go
+import matplotlib
+
+def main():
+
+    # import states shapefile
+    states = gdf.read_file("/Users/kiratsingh/Desktop/research/india_coal/health/input/maps/2011_states.shp")
+
+    # import unit data
+    units = gdf.read_file("/Users/kiratsingh/Desktop/research/india_coal/health/output/visualization_tables/top_25_mort.csv",
+                          index_col=False)
+    units.crs = 'epsg:4326'
+    units.geometry = gdf.points_from_xy(units.longitude, units.latitude)
+    units["sum_delta_M_ij"] = units["sum_delta_M_ij"].astype(float)
+    print(units.columns)
+
+    india_map = states.plot()
+    units.plot(ax=india_map,
+                   column=units["sum_delta_M_ij"],
+                   markersize=units["sum_delta_M_ij"]*0.5,
+                   cmap="inferno_r",
+                   legend=True)
+
+    plt.title('Annual Attributable Premature Mortality per Unit (deaths/year)', fontsize=10)
+    plt.xlabel("Longitude (°E)")
+    plt.ylabel("Latitude (°N)")
+    plt.savefig("/Users/kiratsingh/Documents/coal_health_effects/visualizations/plots/top_25_mort_by_gen.png",
+                dpi=2400)
+
+    plt.show()
+
+main()
